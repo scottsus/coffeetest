8824b5dc40c246fa9a360994031140d796f36c25
scott susanto <susantoscott@gmail.com>
2023-11-07 17:02:31 -0800

    endpoint no cache

--- app/counter-coffee/route.js
+++
@@ -1,8 +0,0 @@
-import { kv } from '@vercel/kv';
-import { NextResponse } from 'next/server';
-
-export async function GET() {
-  const user = await kv.get('coffeecounter');
-  console.log('blah:', user)
-  return NextResponse.json(user);
-}
---
+++ app/counter-coffee/route.ts
@@ -0,0 +1,19 @@
+import { kv } from '@vercel/kv';
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+  const coffeeChatsAided = await kv.get('coffeecounter');
+  console.log('blah:', coffeeChatsAided);
+
+  const res = new NextResponse(JSON.stringify(coffeeChatsAided), {
+    status: 200, // HTTP status code
+    headers: {
+      'Content-Type': 'application/json',
+      'Cache-Control': 'no-cache, no-store, must-revalidate',
+      Pragma: 'no-cache',
+      Expires: '0',
+    },
+  });
+
+  return res;
+}

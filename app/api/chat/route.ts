e0941e6443a07bdfbd2036bb2719e02a71710dee
scott susanto <susantoscott@gmail.com>
2023-11-07 16:03:45 -0800

    blah

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -6,7 +6,6 @@ import { kv } from '@vercel/kv';
 const config = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
-console.log('key:', config.apiKey);
 const openai = new OpenAIApi(config);
 
 // Set the runtime to edge for best performance
--- app/counter-coffee/route.js
+++ app/counter-coffee/route.js
@@ -1,7 +1,8 @@
 import { kv } from '@vercel/kv';
 import { NextResponse } from 'next/server';
- 
+
 export async function GET() {
   const user = await kv.get('coffeecounter');
+  console.log('blah:', user)
   return NextResponse.json(user);
 }
